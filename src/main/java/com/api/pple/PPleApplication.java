1ba5dd101783e8580ef3e2e38ce1a42688544a7a
hayeong25 <hayeong0624@gmail.com>
2024-04-12 16:53:28 +0900

    init

---
+++ src/main/java/com/api/pple/PPleApplication.java
@@ -0,0 +1,11 @@
+package com.api.pple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PPleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PPleApplication.class, args);
+    }
+}
